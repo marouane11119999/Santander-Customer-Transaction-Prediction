6ab7a5ac43d59a802a01e6d3a6d0c8c2601404f9
mar11119999 <marouanesoussi144gmail.com>
2025-08-09 14:09:55 +0200

    project setup

---
+++ readme.md
@@ -0,0 +1,14 @@
+# Santander Customer Transaction Prediction
+
+## 📌 Project Description
+This project develops a **machine learning model** to predict whether a customer will carry out a transaction based on anonymized financial transaction data.  
+The dataset originates from the **Santander Customer Transaction Prediction** Kaggle competition (2019) and is available on **OpenML** (ID: 45566).
+
+**Dataset source:**
+- OpenML dataset: [ID 45566](https://www.openml.org/search?type=data&status=active&id=45566)
+
+---
+
+## 🎯 Objective
+Predict the binary target variable (`target`) that indicates whether a customer will make a transaction, using anonymized numerical features.
+
---
+++ src/data_loader.py
@@ -0,0 +1,17 @@
+from sklearn.datasets import fetch_openml
+import pandas as pd
+import os
+
+def load_data(id :int, location: str) -> pd.DataFrame:
+    """
+    Load dataset from OpenML
+    """
+    if not os.path.exists(location) or not os.path.isdir(location):
+        raise FileNotFoundError(f"Directory not found: {location}")
+    print(f"Loading dataset with id {id} from OpenML...")
+    # Fetch the dataset from OpenML using the provided link
+    dataset = fetch_openml(data_id=id, as_frame=True)
+    # put the dataset into raw folder
+    file_path = os.path.join(location, f"{dataset.details['name']}.csv")
+    dataset.frame.to_csv(path_or_buf=file_path, index=False)
+    return dataset.frame
